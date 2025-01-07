9271a892467c720748471bc459923c8565fe19cb
OMAREID0 <omareid221b@gmail.com>
2025-01-07 16:16:15 +0200

    add user models and use JWT for login

--- store/api/urls.py
+++ store/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import getGameByCategoryid, getGameByCategoryName, add_game, modify_game
+from .views import getGameByCategoryid, getGameByCategoryName, add_game, modify_game, RegisterView, LoginView, UserView, LogoutView
 
 
 
 urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
     path('games/category/<str:name>', getGameByCategoryName, name='byname'),
     path('games/id/<int:id>', getGameByCategoryid, name='byid'),
     path('games/add/', add_game, name='add_game'),
